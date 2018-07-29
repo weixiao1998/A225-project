e23647cf3e2aeacc8b4b261e2c7d054fc6a95f9b
WeiXiao <1197749258@qq.com>
2018-07-29 20:39:21 +0800

    添加代码规范

--- test_tensorflow/test.py
+++ test_tensorflow/test.py
@@ -1,3 +1,5 @@
+# -*-coding:utf-8-*-
+
 import tensorflow as tf
 import numpy as np
 
@@ -8,7 +10,6 @@ def main():
     y_data = np.dot([0.100, 0.200], x_data) + 0.300
 
     # 构造一个线性模型
-    #
     b = tf.Variable(tf.zeros([1]))
     W = tf.Variable(tf.random_uniform([1, 2], -1.0, 1.0))
     y = tf.matmul(W, x_data) + b
